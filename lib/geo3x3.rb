b46acb7e274ca64bdb83c9304bfa956f8e9d0111
Keisuke KITA <kei.kita2501@gmail.com>
2013-02-21 10:50:49 +0900

    implement argument validation

--- lib/geo3x3.rb
+++ lib/geo3x3.rb
@@ -1,4 +1,4 @@
-require "geo3x3/version"
+require File.expand_path("../geo3x3/version", __FILE__)
 
 class Numeric 
   def east_longitude?
@@ -21,34 +21,35 @@ end
 
 class String 
   def east_longitude?
-    self[0] == "E" 
+    self[0] == "E" or self[0] == "+"
   end
 
   def west_longitude?
-    self[0] == "W" 
+    self[0] == "W" or self[0] == "-"
   end
 end
 
 module Geo3x3
 
   def self.encode(params)
-    lat = params[:lat]
-    lng = params[:lng]
+    lat = params[:lat].to_f
+    lng = params[:lng].to_f
     level = params[:level]
+    raise ArgumentError if level < 1
 
     code = 
       if lng.east_longitude?
         "E"
       else
+        lng += 180
         "W"
-        lng = (180-lng)
       end
 
     lat = 90 - lat
 
-    unit = 180 
-    (level-1).times do |i|
-      unit /= 3.0
+    unit = 180.0 
+    (level-1).times do
+      unit /= 3
       x = (lng/unit).floor
       y = (lat/unit).floor
       code += ((x + y*3) + 1).to_s
@@ -61,24 +62,37 @@ module Geo3x3
   end
 
   def self.decode(code)
-    unit = 180
-    lat = 0
-    lng = 0
+    code =
+      case code
+      when Integer
+        code.west_longitude? ? "W#{code}" : "E#{code}"
+      when String
+        raise ArgumentError if code.empty?
+        code
+      else
+        raise ArgumentError
+      end
+
+    unit = 180.0
+    lat = 0.0
+    lng = 0.0
     level = 1
 
     code[1..-1].each_char do |c|
       break if c == "0"
       n = c.to_i
 
-      unit /= 3.0
+      unit /= 3
       n -= 1
       lng += (n % 3) * unit
-      lat += (n / 3).floor * unit
+      lat += (n / 3) * unit
       level += 1
     end
 
-    lng -= 180 if code.west_longitude?
+    lat += unit / 2
+    lng += unit / 2
     lat = 90 - lat
+    lng -= 180 if code.west_longitude?
 
     {
       lat:   lat,
